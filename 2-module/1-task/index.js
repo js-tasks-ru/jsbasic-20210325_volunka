775d27719180c88041540c81a3f109155c8e5839
Konstantin Volynskii <volunka@gmail.com>
2021-04-03 23:44:07 +0300

    Решение задачи 2-1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  let isNotEmpty = false;
+  for (let key in salaries) {
+    if (typeof salaries[key] == 'number' && isFinite(salaries[key]) == true) {
+      isNotEmpty = true;
+      sum += salaries[key];
+    }
+  }
+  if (isNotEmpty == false) {
+    return 0;
+  } else {
+    return sum;
+  }
 }
