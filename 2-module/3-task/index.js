40cc21390688d5bafd3e1597744b86b61d682b79
Konstantin Volynskii <volunka@gmail.com>
2021-04-05 09:44:35 +0300

    Решение задачи 2-3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,31 @@
 let calculator = {
-  // ваш код
+  read(term1, term2) {
+    if (
+      typeof term1 == 'number' &&
+      isFinite(term1) &&
+      typeof term2 == 'number' &&
+      isFinite(term2)
+    ) {
+      this.term1 = term1;
+      this.term2 = term2;
+    } else {
+      return;
+    }
+  },
+  sum() {
+    if (typeof this.term1 == 'undefined' || typeof this.term2 == 'undefined') {
+      return undefined;
+    } else {
+      return this.term1 + this.term2;
+    }
+  },
+  mul() {
+    if (typeof this.term1 == 'undefined' || typeof this.term2 == 'undefined') {
+      return undefined;
+    } else {
+      return this.term1 * this.term2;
+    }
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
