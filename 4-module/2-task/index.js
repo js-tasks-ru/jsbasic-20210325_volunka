1612599fbd389668cf2f28a2688db6094b7d9a97
Konstantin Volynskii <volunka@gmail.com>
2021-04-11 23:58:09 +0300

    Решение задачи 4-1, 4-2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,8 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let incline =
+    (table.rows[table.rows.length - 1].cells.length - 1) / (table.rows.length - 1);
+  for (let i = 0; i < table.rows.length; i++) {
+    let cellIndex = incline * i;
+    table.rows[i].cells[Math.round(cellIndex)].style.backgroundColor = 'red';
+  }
 }
