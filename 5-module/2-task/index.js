5c8af64395140520377c19825e39c55f8f223f2b
Konstantin Volynskii <volunka@gmail.com>
2021-04-18 21:14:41 +0300

    Решение задачи 5-2, 5-3

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,5 @@
 function toggleText() {
-  // ваш код...
+  const hideButton = document.querySelector("button.toggle-text-button");
+  const targetElement = document.getElementById('text');
+  hideButton.addEventListener("click",  () => targetElement.hidden = !targetElement.hidden);
 }
