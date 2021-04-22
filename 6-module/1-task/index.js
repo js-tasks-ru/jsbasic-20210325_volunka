222145c855aa6d9143b3fb6f8fbf39c25cb4b24b
Konstantin Volynskii <volunka@gmail.com>
2021-04-22 11:31:51 +0300

    Решение задачи 6-1, 6-2

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,48 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this._render();
+
   }
+  _render() {
+    const table = document.createElement("table");
+    const tableHead = document.createElement("thead");
+    const tableBody = document.createElement("tbody");
+    let tableRow = document.createElement("tr");
+    let tableData = document.createElement("td");
+
+    // Создаём шапку таблицы
+    tableRow.innerHTML = `<th>Имя</th>
+    <th>Возраст</th>
+    <th>Зарплата</th>
+    <th>Город</th>
+    <th></th>`;
+    tableHead.prepend(tableRow);
+    table.prepend(tableHead);
+    table.append(tableBody);
+
+
+    // Создаём тело таблицы
+    for (let elem of this.rows) {
+      tableRow = document.createElement("tr");
+      tableRow.innerHTML = `
+      <td>${elem.name}</td>
+      <td>${elem.age}</tdh>
+      <td>${elem.salary}</td>
+      <td>${elem.city}</td>
+      <td><button>X</button></td>`;
+      tableBody.append(tableRow);
+    }
+
+
+    document.body.addEventListener("click", (event) => {
+      let target = event.target;
+      if (target.tagName == "BUTTON") {
+        target.closest("tr").remove();
+      }
+    });
+    return table;
+  }
+
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,61 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.elem = this._render();
+
+  }
+  _render() {
+    const divCard = document.createElement('div');
+    const divCardTop = document.createElement('div');
+    const divCardBody = document.createElement('div');
+    const divCardTitle = document.createElement('div');
+    let img = document.createElement('img');
+    const span = document.createElement('span');
+    const button = document.createElement('button');
+
+
+    button.type = "button";
+    button.className = "card__button";
+
+    img.src = `/assets/images/icons/plus-icon.svg`;
+    img.alt = "icon";
+    button.append(img);
+
+    divCardBody.className = "card__body";
+
+    divCardTitle.className = "card__title";
+    divCardTitle.textContent = this.product.name;
+
+    divCardBody.prepend(divCardTitle);
+    divCardBody.append(button);
+
+    img = document.createElement('img');
+    img.src = `/assets/images/products/${this.product.image}`;
+    img.alt = "product";
+    img.className = "card__image";
+
+    span.className = "card__price";
+    span.textContent = `€${this.product.price.toFixed(2)}`;
+
+    divCardTop.className = "card__top";
+    divCardTop.prepend(img);
+    divCardTop.append(span);
+
+    divCard.prepend(divCardTop);
+    divCard.append(divCardBody);
+    divCard.className = "card";
+
+
+    button.onclick = () => {
+      let event = new CustomEvent("product-add", {
+        detail: this.product.id,
+        bubbles: true
+      });
+      divCard.dispatchEvent(event);
+    };
+
+
+    return divCard;
   }
 }
