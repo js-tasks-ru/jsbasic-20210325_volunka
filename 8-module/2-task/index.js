878ed3847b06d3f0172a6acdf7dfe1bbba33bad4
Konstantin Volynskii <volunka@gmail.com>
2021-05-06 19:30:45 +0300

    Попытка исправления ошибок автотеста у задач 8-2 и 8-4

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -24,9 +24,9 @@ export default class ProductGrid {
     const grid = [];
 
     for (const product of this.products) {
-      const oneCard = this._renderOneCard(product);
-      productsGridInner.append(oneCard);
-      grid.push({ product: product, card: oneCard });
+      const oneCard = new ProductCard(product);
+      productsGridInner.append(oneCard.elem);
+      grid.push(oneCard);
     }
 
     this._productsGrid = productsGrid;
@@ -104,68 +104,11 @@ export default class ProductGrid {
 
     function filter(item) {
       if (filterCategory(item) && filterMaxSpiciness(item) && filterVegeterianOnly(item) && filterNoNuts(item)) {
-        productsGridInner.append(item.card);
+        productsGridInner.append(item.elem);
       };
     }
 
     this._gridArray.forEach(filter);
   }
-
-
-
-  _renderOneCard({ name, price, category, image, id, spiciness, nuts }) {
-
-    const divCard = document.createElement('div');
-    const divCardTop = document.createElement('div');
-    const divCardBody = document.createElement('div');
-    const divCardTitle = document.createElement('div');
-    let img = document.createElement('img');
-    const span = document.createElement('span');
-    const button = document.createElement('button');
-
-
-    button.type = "button";
-    button.className = "card__button";
-
-    img.src = `/assets/images/icons/plus-icon.svg`;
-    img.alt = "icon";
-    button.append(img);
-
-    divCardBody.className = "card__body";
-
-    divCardTitle.className = "card__title";
-    divCardTitle.textContent = name;
-
-    divCardBody.prepend(divCardTitle);
-    divCardBody.append(button);
-
-    img = document.createElement('img');
-    img.src = `/assets/images/products/${image}`;
-    img.alt = "product";
-    img.className = "card__image";
-
-    span.className = "card__price";
-    span.textContent = `€${price.toFixed(2)}`;
-
-    divCardTop.className = "card__top";
-    divCardTop.prepend(img);
-    divCardTop.append(span);
-
-    divCard.prepend(divCardTop);
-    divCard.append(divCardBody);
-    divCard.className = "card";
-
-
-    button.onclick = () => {
-      let event = new CustomEvent("product-add", {
-        detail: id,
-        bubbles: true
-      });
-      divCard.dispatchEvent(event);
-    };
-    return divCard;
-  }
-
-
 
 }
